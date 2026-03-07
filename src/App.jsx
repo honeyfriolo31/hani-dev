3b47fbd441887190db49a75c3ed22881d49c29c1
honeyfriolo31 <hgfriolo@gmail.com>
2026-03-07 19:50:08 +0400

    Adding page router and proper project structuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState } from 'react';
+// This line imports routing tools from React Router, which is used to create navigation in a React application.
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//Import the Pages
+import { About, Contact, Home, Projects } from './pages'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500 flex justify-center ">HELLO HANI WORLD</h1>
-    
+    {/* Install this "npm i react-router-dom" and "npm install react-icons --save" */}
+    {/* Create a Router links for the in the App.jsx */}
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='projects' element={<Projects />} />
+        <Route path='about' element={<About/>} />
+      </Routes>
+    </Router>
     </>
   )
 }
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+// All the created linked pages should be exported 
+export { default as Home } from './Home.jsx'
+export { default as About } from './About.jsx'
+export { default as Contact } from './Contact.jsx'
+export { default as Projects } from './Projects.jsx'
