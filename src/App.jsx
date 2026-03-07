e4caaa2d363ed82672e106c85a0e806652c465bf
honeyfriolo31 <hgfriolo@gmail.com>
2026-03-07 11:40:02 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500 flex justify-center ">HELLO HANI WORLD</h1>
+    
+    </>
+  )
+}
+
+export default App
