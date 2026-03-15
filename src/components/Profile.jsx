0ed69eeab88ff02cf4eeca5b3f1d14bbe2043457
honeyfriolo31 <hgfriolo@gmail.com>
2026-03-15 23:30:10 +0400

    Applying the responsive learnings

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,48 +4,48 @@ import profilepic from "../assets/images/honey-profile.jpg"
 
 const Profile = () => {
     return (
-        <section className='mb-8 animate-fade-in'>
-            <div className='flex items-center gap-4 md:gap-6'>
+        <section className='mb-8 animate-fade-in. border border-blue-500'>
+            <div className='flex items-center gap-4 md:gap-6 border border-yellow-500'>
 
                 {/* Main Content */}
                 {/* Image */}
-                <img src={profilepic} alt="Profile" className="w-40 h-40 md:w-40 md:h-40 flex-shrink-0 object-cover" />
+                <img src={profilepic} alt="Profile" className="w-40 h-40 md:w-40 md:h-40 flex-shrink-0 object-cover " />
 
                 {/* flex-1 allow a flex item to grow and shrink as needed */}
-                <div className='flex-1 min-w-0 '>
+                <div className='flex-1 min-w-0 border border-orange-500'>
 
                     <div className='flex gap-2'>
                         {/* Name */}
-                        <h1 className="text-base md:text-2xl font-bold">
+                        <h1 className="text-lg md:text-2xl font-bold">
                             Honey Grace Friolo
                         </h1>
                         <span className="text-blue-500"><CiBookmarkCheck size={25} /></span>
                     </div>
 
                     {/* Location */}
-                    <div className='flex md:mt-2'>
-                        <CiLocationOn size={18} />
-                        <span className='text-xs md:text-sm'>Abu Dhabi, United Arab Emirates</span>
+                    <div className='flex md:mt-2 border border-orange-500 items-center gap-1'>
+                        <CiLocationOn size={22} />
+                        <p className='text-xs md:text-lg '>Abu Dhabi, United Arab Emirates</p>
                     </div>
 
 
                     {/* Roles */}
-                    <p className="flex text-xs md:text-sm mt-2">
+                    <div className="flex text-[15px] md:text-xl md:mt-2 ">
                         Front-end Developer / QA Engineer
-                    </p>
+                    </div>
 
                     {/* Buttons */}
 
-                    <div className='flex gap-3 md:space-x-1 md:mt-4 mt-2'>
+                    <div className='flex space-x-1 md:gap-3 md:space-x-1 md:mt-2 mt-2 flex-shrink-0'>
 
                         {/* Email */}
-                        <button className='px-3 py-2 border rounded-md hover:scale-105 transition duration-300 '>
+                        <button className='px-3 py-1 border rounded-md hover:scale-105 transition duration-300 '>
                             <a href="mailto:hgfriolo@gmail.com" className="flex text-xs md:text-sm gap-2">
                                 <CiMail size={18} /> Email
                             </a>
                         </button>
 
-                        <button className='bg-white border px-3 py-2 rounded-md hover:scale-105 transition duration-300 hidden md:flex'>
+                        <button className='bg-white border px-3 py-2 sm:px-2 sm:py-2 rounded-md hover:scale-105 transition duration-300 hidden md:flex'>
                             {/* LinkedIn */}
                             <a href='https://www.linkedin.com/in/honey-grace-friolo-1802b2289/' target='_blank' className="flex text-xs md:text-sm gap-2">
                                 <CiLinkedin size={18} />
@@ -53,7 +53,7 @@ const Profile = () => {
                             </a>
                         </button>
 
-                        <button className='bg-white border px-3 py-2 rounded-md hover:scale-105 transition duration-300'>
+                        <button className='bg-white border px-3 py-1 sm:px-2 sm:py-2 rounded-md hover:scale-105 transition duration-300'>
                             {/* Project */}
                             <a href='#projects' className="flex text-xs md:text-sm gap-2">
                                 <CiBoxList size={18} />
